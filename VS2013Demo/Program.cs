feb187ffc6b8380c0a9760c3982272de48eda6cb
lsandler <lsandler@vancepublishing.com>
2015-03-03 14:30:10 -0600

    initial Project comit

---
+++ VS2013Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2013Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
